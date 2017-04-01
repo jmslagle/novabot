70d3d5441cb8772c7a897de48075753c62050b7c
David Mcmurray <David Mcmurray>
2017-04-01 16:28:32 +1300

    Back To Orginal, sorry

--- src/Region.java
+++ src/Region.java
@@ -2,68 +2,118 @@
  * Created by Paris on 18/03/2017.
  */
 public enum Region {
-    rares,
-    ninetyfiveiv,
-    hundrediv,
-    simple_rares,
-    mareepandevolutions,
-    larvitarandevolutions,
-    dratiniandevolutions,
-    unown,
-    All;
+    Innernorth,
+    Innersouth,
+    Gungahlin,
+    Belconnen,
+    Wodenweston,
+    Tuggeranong,
+    Queanbeyan,
+    Hundrediv,
+    Dratini,
+    Mareep,
+    Snorlax,
+    Larvitar,
+    Legacyrare,
+    Ultrarare,
+    All,
+    Event,
+    Regional;
 
     public static Region fromString(String regionStr) {
         if (regionStr.equals("all"))
             return All;
 
+        if(regionStr.equals("regional"))
+            return Regional;
+
         if (regionStr.equals("woden-weston") || regionStr.equals("wodenweston") || regionStr.equals("woden-weston-supporter")
                 || regionStr.equals("woden-weston-region") || regionStr.equals("woden/weston"))
-            return mareepandevolutions;
+            return Wodenweston;
 
         if (regionStr.equals("belconnen") || regionStr.equals("belconnen-supporter") || regionStr.equals("belconnen-region"))
-            return simple_rares;
+            return Belconnen;
 
         if (regionStr.equals("innernorth") || regionStr.equals("inner-north") || regionStr.equals("inner-north-supporter")
                 || regionStr.equals("inner-north-region") || regionStr.equals("inner north"))
-            return rares;
+            return Innernorth;
 
         if (regionStr.equals("innersouth") || regionStr.equals("inner-south") || regionStr.equals("inner-south-supporter")
                 || regionStr.equals("inner-south-region") || regionStr.equals("inner south"))
-            return ninetyfiveiv;
+            return Innersouth;
 
         if (regionStr.equals("gungahlin") || regionStr.equals("gungahlin-region") || regionStr.equals("gungahlin-supporter"))
-            return hundrediv;
+            return Gungahlin;
 
         if (regionStr.equals("tuggeranong") || regionStr.equals("tuggeranong-region") || regionStr.equals("tuggeranong-supporter"))
-            return larvitarandevolutions;
+            return Tuggeranong;
 
         if (regionStr.equals("queanbeyan") || regionStr.equals("queanbeyan-region") || regionStr.equals("queanbeyan-supporter"))
-            return dratiniandevolutions;
+            return Queanbeyan;
+
+        if (regionStr.equals("legacy") || regionStr.equals("legacyrare") || regionStr.equals("legacy-rare")
+                || regionStr.equals("legacy-rare-supporters"))
+            return Legacyrare;
+
+        if (regionStr.equals("larvitar") || regionStr.equals("larvitarcandy") || regionStr.equals("larvitar-candy")
+                || regionStr.equals("larvitar-candy-supporter"))
+            return Larvitar;
+
+        if (regionStr.equals("dratini") || regionStr.equals("dratinicandy") || regionStr.equals("dratini-candy")
+                || regionStr.equals("dratini-candy-supporter"))
+            return Dratini;
+
+        if (regionStr.equals("mareep") || regionStr.equals("mareepcandy") || regionStr.equals("mareep-candy")
+                || regionStr.equals("mareep-candy-supporter"))
+            return Mareep;
+
+        if (regionStr.equals("ultrarare") || regionStr.equals("ultra-rare") || regionStr.equals("ultra-rare-supporter"))
+            return Ultrarare;
 
         if (regionStr.equals("100iv") || regionStr.equals("100%") || regionStr.equals("100-iv-supporter") || regionStr.equals("100-iv"))
-            return unown;
+            return Hundrediv;
+
+        if (regionStr.equals("snorlax") || regionStr.equals("snorlax-supporter"))
+            return Snorlax;
+
+        if (regionStr.equals("event"))
+            return Event;
 
         return null;
     }
 
     public String toWords() {
         switch (this) {
-            case rares:
+            case Innernorth:
                 return "inner north";
-            case ninetyfiveiv:
+            case Innersouth:
                 return "inner south";
-            case hundrediv:
+            case Gungahlin:
                 return "gungahlin";
-            case simple_rares:
+            case Belconnen:
                 return "belconnen";
-            case mareepandevolutions:
+            case Wodenweston:
                 return "woden-weston";
-            case larvitarandevolutions:
+            case Tuggeranong:
                 return "tuggeranong";
-            case dratiniandevolutions:
+            case Queanbeyan:
                 return "queanbeyan";
-            case unown:
-                return "Unown";
+            case Hundrediv:
+                return "100% iv";
+            case Dratini:
+                return "dratini candy";
+            case Mareep:
+                return "mareep candy";
+            case Snorlax:
+                return "snorlax";
+            case Larvitar:
+                return "larvitar candy";
+            case Legacyrare:
+                return "legacy rare";
+            case Ultrarare:
+                return "ultra rare";
+            case Event:
+                return "event";
             case All:
                 return "all";
         }
