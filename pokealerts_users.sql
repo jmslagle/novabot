cb3cd080adc633dd23cf2ff35df1af366009a9d5
parislarkins <paris.larkins@gmail.com>
2017-04-06 08:16:32 +1000

    Added DB ddl/insert scripts for the test DB

---
+++ pokealerts_users.sql
@@ -0,0 +1,12 @@
+INSERT INTO pokealerts.users (id, joindate) VALUES ('10773087559616', null);
+INSERT INTO pokealerts.users (id, joindate) VALUES ('107730875596169216', null);
+INSERT INTO pokealerts.users (id, joindate) VALUES ('10773087559617', null);
+INSERT INTO pokealerts.users (id, joindate) VALUES ('123456', '2017-04-03 09:39:25');
+INSERT INTO pokealerts.users (id, joindate) VALUES ('127376920076615680', null);
+INSERT INTO pokealerts.users (id, joindate) VALUES ('137392784691822592', null);
+INSERT INTO pokealerts.users (id, joindate) VALUES ('271808515897688064', null);
+INSERT INTO pokealerts.users (id, joindate) VALUES ('280150791631798283', '2017-04-03 16:41:58');
+INSERT INTO pokealerts.users (id, joindate) VALUES ('295841417870442497', null);
+INSERT INTO pokealerts.users (id, joindate) VALUES ('298330902696558592', '2017-04-03 15:41:03');
+INSERT INTO pokealerts.users (id, joindate) VALUES ('298332071666122765', '2017-04-03 16:15:48');
+INSERT INTO pokealerts.users (id, joindate) VALUES ('298343107664019466', '2017-04-03 16:41:42');
